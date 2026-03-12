2f09eebe2caf89f30fa1952c30aef6a6b8546cb7
thivernale <thivernale@gmail.com>
2026-03-12 14:45:25 +0200

    Line separator

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'todos', pathMatch: 'full' },
+  { path: 'todos', loadComponent: () => TodosComponent },
+];
